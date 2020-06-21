f85d90963a17cb187e42f2ea992b3b6b60140f2c
Yangfan Zhang <yf_chang@icloud.com>
2020-06-21 14:17:25 +1000

    Passed kindOfDay app.js into list.ejs

--- Web Development/todolist-v1/app.js
+++ Web Development/todolist-v1/app.js
@@ -2,16 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     var today = new Date();
     var currentDay = today.getDay();
+    var day ="";
 
     if(currentDay === 6 || currentDay === 0){
-        res.write("<h1>Yay it's the weekend!</h1>");
+        day = "Weekend";
     } else {
-        res.sendFile(__dirname + "/index.html");
+        day = "Weekday";
     }
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function(){
